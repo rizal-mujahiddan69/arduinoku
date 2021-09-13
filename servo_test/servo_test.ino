397e8b4e69f8d916802631f14d204815d4363bac
rizal-mujahiddan69 <86094348+rizal-mujahiddan69@users.noreply.github.com>
2021-09-13 09:51:30 +0700

    adding comment

--- servo_test/servo_test.ino
+++ servo_test/servo_test.ino
@@ -1,14 +1,14 @@
 #include<Servo.h>
 Servo servo;
 void setup() {
+  // pin A5 buat potensialmeter
   pinMode(A5,"INPUT");
+  // Pin 9 buat servo
   servo.attach(9);
   Serial.begin(9600);
-  // put your setup code here, to run once:
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   int hasil = map(analogRead(A5),0,1023,0,180);
   Serial.println(hasil);
   servo.write(hasil);
